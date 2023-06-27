f9d229ff6d7ef49e7aa7d8d2a4ee67c1d3139b80
buriedsignals <tom.vaillant@icloud.com>
2023-06-27 11:41:18 +0200

    first version with Flourish embedded

---
+++ components/FlourishEmbed1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FlourishEmbed1 = () => (
+    <iframe 
+      src="https://public.flourish.studio/story/1957820/?full" 
+      style={{ 
+        border: 'none', 
+        width: '100%', 
+        height: '800px' // Adjust this value as needed
+      }} 
+    />
+  );
+
+export default FlourishEmbed1;
